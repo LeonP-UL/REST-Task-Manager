70bd7db0f3cc6293dcc8bf2cd138c3ac28722d3f
LPfahl-UL <leonpfahl2002@gmail.com>
2026-06-03 18:49:35 +0200

    Add User Entity and Table

---
+++ src/main/java/com/gamepil/task/entity/User.java
@@ -0,0 +1,52 @@
+package com.gamepil.task.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id //Primary Key
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUserName(String name) {
+        this.username = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
